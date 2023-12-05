27b170810d7d47fda17e6799739d28b7966131c3
hhuezo <hugo.alex.huezo@gmail.com>
2023-12-05 11:21:36 -0600

    datos iglesia

--- app/Http/Controllers/administracion/DatosIglesiaController.php
+++ app/Http/Controllers/administracion/DatosIglesiaController.php
@@ -14,38 +14,33 @@ class DatosIglesiaController extends Controller
 
     public function index(Request $request)
     {
-
-        try{
+        try {
             $user   = User::findOrFail(auth()->user()->id);
-            // dd($user);
-             $iglesia = $user->user_has_iglesia->first();
+            $iglesia = $user->user_has_iglesia->first();
 
-             $departamentos = Departamento::get();
-             $grupos_iglesia = $iglesia->iglesia_has_grupo;
-
-
-             $url =  url('/') . "/registro_participantes/" . $iglesia->id . '/';
-
-             foreach ($grupos_iglesia as $obj) {
-                 $obj->conteo = $iglesia->countMembers($iglesia->id, $obj->id);
-                 $obj->codigo_qr = 'qrcodeiglesiagrupo' . $obj->id . '.png';
-                 QrCode::format('png')->size(200)->generate($url . '/' . $obj->id, public_path('img/qrcodeiglesiagrupo' . $obj->id . '.png'));
-             }
+            $departamentos = Departamento::get();
+            $grupos_iglesia = $iglesia->iglesia_has_grupo;
 
 
-             QrCode::format('png')->size(200)->generate($url . '0', public_path('img/qrcodeiglesia.png'));
+            $url =  url('/') . "/registro_participantes/" . $iglesia->id . '/';
 
-             return view('catalog.iglesia.datos_iglesia', compact(
-                 'departamentos',
-                 'iglesia',
-                 'grupos_iglesia'
-             ));
-        }
-        catch(Exception $e){
+            foreach ($grupos_iglesia as $obj) {
+                $obj->conteo = $iglesia->countMembers($iglesia->id, $obj->id);
+                $obj->codigo_qr = 'qrcodeiglesiagrupo' . $obj->id . '.png';
+                QrCode::format('png')->size(200)->generate($url . '/' . $obj->id, public_path('img/qrcodeiglesiagrupo' . $obj->id . '.png'));
+            }
+
+
+            QrCode::format('png')->size(200)->generate($url . '0', public_path('img/qrcodeiglesia.png'));
+
+            return view('datos_iglesia.index', compact(
+                'departamentos',
+                'iglesia',
+                'grupos_iglesia'
+            ));
+        } catch (Exception $e) {
             alert()->error('Error Datos No Coinciden');
             return back();
         }
-
     }
-
 }
