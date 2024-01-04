79840263c31f6ee33c23bd7aaffaefc31b5237f3
hhuezo <hugo.alex.huezo@gmail.com>
2024-01-04 15:36:44 -0600

    colores generales

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/SimpleBar.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/rt-plugins.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/general_colors.css') }}">
     <!-- END: Theme CSS-->
     <script src="{{ asset('assets/js/settings.js') }}" sync></script>
     <script src="{{ asset('assets/js/iconify-icon.min.js') }}"></script>
@@ -34,78 +35,6 @@
     });
     </script>
 
-    <style>
-    .card-title,
-    .form-label {
-        text-transform: none;
-    }
-
-    /* .btn-dark {
-            background-color: #740816;
-        } */
-
-    .switch {
-        position: relative;
-        display: inline-block;
-        width: 60px;
-        height: 34px;
-    }
-
-    /* Hide default HTML checkbox */
-    .switch input {
-        opacity: 0;
-        width: 0;
-        height: 0;
-    }
-
-    /* The slider */
-    .slider {
-        position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #ccc;
-        -webkit-transition: .4s;
-        transition: .4s;
-    }
-
-    .slider:before {
-        position: absolute;
-        content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        -webkit-transition: .4s;
-        transition: .4s;
-    }
-
-    input:checked+.slider {
-        background-color: #2196F3;
-    }
-
-    input:focus+.slider {
-        box-shadow: 0 0 1px #2196F3;
-    }
-
-    input:checked+.slider:before {
-        -webkit-transform: translateX(26px);
-        -ms-transform: translateX(26px);
-        transform: translateX(26px);
-    }
-
-    /* Rounded sliders */
-    .slider.round {
-        border-radius: 34px;
-    }
-
-    .slider.round:before {
-        border-radius: 50%;
-    }
-    </style>
 
 </head>
 
@@ -160,7 +89,7 @@
                     <li class="">
                         <a href="#" class="navItem">
                             <span class="flex items-center">
-                                <iconify-icon class=" nav-icon" icon="heroicons-outline:user"></iconify-icon>
+                                <iconify-icon class="nav-icon" icon="heroicons-outline:user"></iconify-icon>
                                 <span>Seguridad</span>
                             </span>
                             <iconify-icon class="icon-arrow" icon="heroicons-outline:chevron-right"></iconify-icon>
