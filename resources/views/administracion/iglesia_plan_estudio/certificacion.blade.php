74b91b28d53229d6554e8ca84e438245a03e2731
Mar Escobar <mar.escobar1991@gmail.com>
2024-01-05 16:32:54 -0600

    certificacion de iglesias

--- resources/views/administracion/iglesia_plan_estudio/certificacion.blade.php
+++ resources/views/administracion/iglesia_plan_estudio/certificacion.blade.php
@@ -1,113 +1,79 @@
 @extends ('menu')
 @section('contenido')
 <div class="grid grid-cols-12 gap-5 mb-5">
-    @php($validar_sesiones = 0)
-    @php($total_sesiones = 0)
-    @foreach($planes as $plan)
-        @php($total_sesiones = $total_sesiones + $plan->sesiones->count())
-        @php($validar_sesiones = $validar_sesiones +  $plan->sesiones->where('completed','=',1)->count()) 
-    @endforeach
-    @if($validar_sesiones == $total_sesiones)
-    <div class="2xl:col-span-12 lg:col-span-12 col-span-12">
-        <div class="card">
-            <div class="card-body flex flex-col p-6">
-            <label class="card-title">Certificación de Iglesia</label>
-                <div class="flex-1">
-                    
-                    <div class="card-title text-slate-900 dark:text-white" align="right" style="margin-top: -25px;">
-                        @if($iglesia->validar_asistencias($iglesia->id) >= 13 && $iglesia->validar_asistencias_jovenes($iglesia->id) == 0)
-                        <a href="{{url('administracion/iglesia_plan_estudio/certificado')}}/{{$iglesia->id}}" class="btn btn-success">Certificación de Iglesia</a>
-                        @else
-                        <a href="{{url('administracion/iglesia_plan_estudio/certificado_participante')}}/{{$iglesia->id}}" class="btn btn-success">Certificación de Participantes</a>
-                        @endif
-                    </div>
-                </div>
 
-            </div>
-        </div>
-    </div>
-    @else
-    <div class="2xl:col-span-12 lg:col-span-12 col-span-12">
-        <div class="card">
-            <div class="card-body flex flex-col p-6">
-            <label class="card-title">Certificación de Iglesia</label>
-                <div class="flex-1">
-                    <div class="card-title text-slate-900 dark:text-white" align="center" style="margin-top: -25px;">
-                       <label class="btn btn-outline-danger">Falta Completar Sesiones</label> 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    @endif
-
-    @foreach ($planes as $plan)
 
     <div class="2xl:col-span-12 lg:col-span-12 col-span-12">
+
         <div class="card">
             <div class="card-body flex flex-col p-6">
                 <header class="flex mb-5 items-center border-b border-slate-100 dark:border-slate-700 pb-5 -mx-6 px-6">
                     <div class="flex-1">
-                        <div class="card-title text-slate-900 dark:text-white">{{ $plan->grupo->nombre }}
-
-                        </div>
+                        <div class="card-title text-slate-900 dark:text-white">Certificación</div>
                     </div>
                 </header>
+                <div class="card-text h-full ">
+                    <div>
+                        <ul class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4" id="tabs-tab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a href="#tabs-home" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent active dark:text-slate-300" id="tabs-home-tab" data-bs-toggle="pill" data-bs-target="#tabs-home" role="tab" aria-controls="tabs-home" aria-selected="true">Iglesias Certificadas</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a href="#tabs-profile" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent dark:text-slate-300" id="tabs-profile-tab" data-bs-toggle="pill" data-bs-target="#tabs-profile" role="tab" aria-controls="tabs-profile" aria-selected="false">Iglesias no Certificadas</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="tabs-tabContent">
 
-                <div class="transition-all duration-150 container-fluid" id="page_layout">
-                    <div id="content_layout">
-                        <div class="space-y-5">
-                            <div class="grid grid-cols-12 gap-5">
+                            <div class="tab-pane fade show active" id="tabs-home" role="tabpanel" aria-labelledby="tabs-home-tab">
+                                <header class="flex mb-5 items-center border-b border-slate-100 dark:border-slate-700 pb-5 -mx-6 px-6">
+                                    <div class="flex-1">
+                                        <div class="card-title text-slate-900 dark:text-white">Listado de Iglesias</div>
+                                    </div>
+                                </header>
+                                <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
+                                    <thead class="bg-slate-200 dark:bg-slate-700">
+                                        <tr>
+                                            <th scope="col" class=" table-th ">
+                                                Nombre
+                                            </th>
+                                            <th scope="col" class=" table-th ">
+                                                Sede
+                                            </th>
+                                            <th scope="col" class=" table-th ">
+                                                Cohort
+                                            </th>
+                                            <th scope="col" class=" table-th ">
+                                                Region
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
 
-                                <div class="xl:col-span-12 col-span-12 lg:col-span-6">
-
-
-                                    <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
-                                        <thead class="bg-slate-200 dark:bg-slate-700">
-                                            <tr>
-                                                <th scope="col" class=" table-th ">
-                                                    Sesion
-                                                </th>
-                                                <th scope="col" class=" table-th ">
-                                                    Temas
-                                                </th>
-                                                <th scope="col" class=" table-th ">
-                                                    Asistencias por <br> Participantes
-                                                </th>
-                                                <th scope="col" class=" table-th ">
-                                                    Completada
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
-
-                                            @foreach ($plan->sesiones as $sesion)
-                                            <tr class="even:bg-slate-50 dark:even:bg-slate-700">
-                                                <td class="table-td">{{ $sesion->session_name }}</td>
-                                                <td class="table-td">{{ implode(', ', $sesion->detalles->pluck('curso.name_es')->toArray()) }} </td>
-                                                <td class="table-td">{{ $sesion->asistencias->where('attended','=',1)->count()}}/{{ $sesion->asistencias->count()}} </td>
-                                                <td class="table-td " >
-                                                    <div class="checkbox-area">
-                                                        <label class="inline-flex items-center cursor-pointer">
-                                                            <input type="checkbox" class="hidden" name="checkbox" {{ $sesion->completed == 1 ? 'checked' : '' }}>
-                                                            <span class="h-4 w-4 border flex-none border-slate-100 dark:border-slate-800 rounded inline-flex ltr:mr-3 rtl:ml-3 relative transition-all duration-150 bg-slate-100 dark:bg-slate-900">
-                                                                <img src="{{asset('assets/images/icon/ck-white.svg')}}" alt="" class="h-[10px] w-[10px] block m-auto opacity-0"></span>
-
-                                                        </label>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        @foreach ($iglesias as $iglesia)
+                                        <tr class="even:bg-slate-50 dark:even:bg-slate-700">
+                                            <td class="table-td">{{$iglesia->nombreIglesia}}</td>
+                                            <td class="table-td">{{$iglesia->sede_id == null ? '': $iglesia->sedeiglesia->nombre}}</td>
+                                            <td class="table-td">{{$iglesia->sede_id == null ? '': $iglesia->sedeiglesia->cohorte->nombre}}</td>
+                                            <td class="table-td">{{$iglesia->sede_id == null ? '': $iglesia->sedeiglesia->cohorte->region->nombre}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
+                            <div class="tab-pane fade" id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
+                                <p class="text-sm text-gray-500 dark:text-gray-200">
+                                    This is some placeholder content the
+                                    <strong>Profile</strong>
+                                    tab's associated content. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling. consectetur adipisicing elit. Ab ipsa!
+                                </p>
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endforeach
+
 </div>
 @endsection
